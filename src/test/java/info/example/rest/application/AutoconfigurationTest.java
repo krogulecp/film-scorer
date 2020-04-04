d92e27edd79d9cdab565b2a0fb0d319f94a6ca36
Son <son@sonit.info>
2020-04-04 17:34:12 +0200

    test spring context with loaded configuration

---
+++ src/test/java/info/example/rest/application/AutoconfigurationTest.java
@@ -0,0 +1,26 @@
+package info.example.rest.application;
+
+import info.example.starter.ApplicationPreConfiguration;
+import info.example.starter.ApplicationConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AutoconfigurationTest {
+
+    private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withConfiguration(AutoConfigurations.of(info.example.rest.application.ApplicationConfiguration.class))
+            .withConfiguration(AutoConfigurations.of(ApplicationConfiguration.class))
+            .withConfiguration(AutoConfigurations.of(ApplicationPreConfiguration.class));
+
+    @Test
+    void should_find_bean_in_context() {
+        this.contextRunner.run((context -> {
+            assertThat(context).hasBean("mapperNumber");
+        }));
+    }
+}
