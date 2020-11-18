a0575cd954579c7d50e8082c89149e2d8a35d769
Piotr Krogulec <piotr.krogulec@allegro.pl>
2020-11-18 13:23:17 +0100

    small fixes

--- src/main/java/info/example/rest/FilmScorerApplication.java
+++ src/main/java/info/example/rest/FilmScorerApplication.java
@@ -1,4 +1,4 @@
-package info.example.rest.application;
+package info.example.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
